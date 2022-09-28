e905c3634981b686793eb0c76c2f5c863553bd5b
r7mce <mert1997can@gmail.com>
2022-09-28 13:25:19 +0200

    Add files via upload

--- Staking.sol
+++ Staking.sol
@@ -101,96 +101,34 @@ abstract contract ReentrancyGuard {
 }
 
 interface IERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
     event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
     function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
     function balanceOf(address account) external view returns (uint256);
-
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
     function transfer(address to, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
     function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
     function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `from` to `to` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
     function transferFrom(
         address from,
         address to,
         uint256 amount
     ) external returns (bool);
 }
-
 contract Stake is Pausable, Ownable, ReentrancyGuard {
     IERC20 XOTToken;
-    //  Days (16 * 30 * 24 * 60 * 60)
-    uint256 public Duration = 41472000;
-    uint8 public interestRate = 25 ;
+    uint256 public Duration = 41472000;    //  Days (16 * 30 * 24 * 60 * 60)
     uint8 public totalStakers;
     uint256 rewardAmount ;
     mapping (address => uint256 ) _balances;
     struct StakeInfo {        
-        uint256 startTS;
-        uint256 endTS;        
+        uint256 startTime;
+        uint256 endTime;        
         uint256 amount; 
         uint256 claimed;       
     }
     event Staked(address indexed from, uint256 amount);
     event Claimed(address indexed from, uint256 amount);
+    event AddRewardTokens(uint256);
     mapping(address => StakeInfo) public stakeInfos;
     mapping(address => bool) public addressStaked;
     constructor(IERC20 _tokenAddress,uint256 amount) {
@@ -203,18 +141,16 @@ contract Stake is Pausable, Ownable, ReentrancyGuard {
     }    
     function claimReward() external returns (bool){
         require(addressStaked[_msgSender()] == true, "You are not participated");
-        require(stakeInfos[_msgSender()].endTS < block.timestamp, "Stake Time is not over yet");
+        require(stakeInfos[_msgSender()].endTime < block.timestamp, "Stake Time is not over yet");
         require(stakeInfos[_msgSender()].claimed == 0, "Already claimed");
-        uint256 stakeAmount = stakeInfos[_msgSender()].amount;
-        uint256 totalTokens = stakeAmount + (stakeAmount * interestRate / 1000);
-        stakeInfos[_msgSender()].claimed == totalTokens;
-        XOTToken.transfer(_msgSender(), totalTokens);
-        emit Claimed(_msgSender(), totalTokens);
+        stakeInfos[_msgSender()].claimed ==  stakeInfos[_msgSender()].amount + (stakeInfos[_msgSender()].amount * 25 / 1000);
+        XOTToken.transfer(_msgSender(),  stakeInfos[_msgSender()].amount + (stakeInfos[_msgSender()].amount * 25 / 1000));
+        emit Claimed(_msgSender(),  stakeInfos[_msgSender()].amount + (stakeInfos[_msgSender()].amount * 25 / 1000));
         return true;
     }
     function getTokenExpiry() external view returns (uint256) {
         require(addressStaked[_msgSender()] == true, "You are not participated");
-        return stakeInfos[_msgSender()].endTS;
+        return stakeInfos[_msgSender()].endTime;
     }
     function stakeToken(uint256 stakeAmount) external whenNotPaused {
         require(stakeAmount >0, "Stake amount should be correct");
@@ -224,12 +160,11 @@ contract Stake is Pausable, Ownable, ReentrancyGuard {
             totalStakers++;
             addressStaked[_msgSender()] = true;
             stakeInfos[_msgSender()] = StakeInfo({                
-                startTS: block.timestamp,
-                endTS: block.timestamp + Duration,
+                startTime: block.timestamp,
+                endTime: block.timestamp + Duration,
                 amount: stakeAmount,
                 claimed: 0
             });
-        
         emit Staked(_msgSender(), stakeAmount);
     }    
     function pause() external onlyOwner {
@@ -238,9 +173,10 @@ contract Stake is Pausable, Ownable, ReentrancyGuard {
     function unpause() external onlyOwner {
         _unpause();
     }
-    function transferreward(uint256 amount) public onlyOwner{
+    function AddRewardToken(uint256 amount) public onlyOwner{
         rewardAmount = rewardAmount + amount;
         XOTToken.transferFrom(msg.sender, address(this),rewardAmount) ; 
         _balances[address(this)] = rewardAmount;
+        emit AddRewardTokens(amount);
     }
 }
